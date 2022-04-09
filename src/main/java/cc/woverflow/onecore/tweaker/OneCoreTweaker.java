8d93b74d13b108b6bb2b9c125a1d33a0719bf8aa
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-04-09 14:05:55 +0900

    uh somehow i didnt push this

--- src/main/java/cc/woverflow/onecore/tweaker/OneCoreTweaker.java
+++ src/main/java/cc/woverflow/onecore/tweaker/OneCoreTweaker.java
@@ -28,7 +28,7 @@ public class OneCoreTweaker extends EssentialSetupTweaker {
                 null,
                 "OneCore has failed to download!\n" +
                         "This may be because the servers are down.\n" +
-                        "For more information, please join our discord server: https://woverflow.cc/discord\n" +
+                        "For more information, please join our discord server (https://woverflow.cc/discord) and go to #support-bug-report\n" +
                         "or try again later.",
                 "OneCore has failed!", JOptionPane.ERROR_MESSAGE
         );
@@ -45,58 +45,63 @@ public class OneCoreTweaker extends EssentialSetupTweaker {
     @Override
     public void injectIntoClassLoader(LaunchClassLoader classLoader) {
         super.injectIntoClassLoader(classLoader);
-        if (Launch.blackboard.get("onecore") == null) {
-            File loadLocation = new File(new File(new File(Launch.minecraftHome, "W-OVERFLOW"), "OneCore"), "OneCore-Loader.jar");
-            JsonObject json = null;
-            try {
-                if (!loadLocation.getParentFile().exists()) loadLocation.getParentFile().mkdirs();
-                String theJson = InternetUtils.getStringOnline("https://woverflow.cc/static/data/onecore.json");
-                if (theJson == null) {
-                    if (!loadLocation.exists()) {
-                        showErrorScreen();
-                    } else {
-                        URL fileURL = loadLocation.toURI().toURL();
-                        if (!Launch.classLoader.getSources().contains(fileURL)) {
-                            Launch.classLoader.addURL(fileURL);
-                        }
-                        loader = ((ITweaker) Launch.classLoader.findClass("cc.woverflow.onecore.loader.OneCoreLoader").newInstance());
-                    }
-                }
-                if (theJson != null) {
-                    json = new JsonParser().parse(theJson).getAsJsonObject();
-                    if (json.has("loader")) {
-                        if (!loadLocation.exists() || !InternetUtils.getChecksumOfFile(loadLocation.getPath()).equals(json.get("checksum_loader").getAsString())) {
-                            System.out.println("Downloading / updating OneCore loader...");
-                            if (!InternetUtils.download(json.get("loader").getAsString(), loadLocation)) {
-                                if (!loadLocation.exists()) {
-                                    showErrorScreen();
-                                }
-                            }
-                        }
-                    } else {
-                        // oh
+        try {
+            if (Launch.blackboard.get("onecore") == null) {
+                File loadLocation = new File(new File(new File(Launch.minecraftHome, "W-OVERFLOW"), "OneCore"), "OneCore-Loader.jar");
+                JsonObject json = null;
+                try {
+                    if (!loadLocation.getParentFile().exists()) loadLocation.getParentFile().mkdirs();
+                    String theJson = InternetUtils.getStringOnline("https://woverflow.cc/static/data/onecore-all.json");
+                    if (theJson == null) {
                         if (!loadLocation.exists()) {
                             showErrorScreen();
+                        } else {
+                            URL fileURL = loadLocation.toURI().toURL();
+                            if (!Launch.classLoader.getSources().contains(fileURL)) {
+                                Launch.classLoader.addURL(fileURL);
+                            }
+                            loader = ((ITweaker) Launch.classLoader.findClass("cc.woverflow.onecore.loader.OneCoreLoader").newInstance());
                         }
                     }
+                    if (theJson != null) {
+                        json = new JsonParser().parse(theJson).getAsJsonObject();
+                        if (json.has("loader")) {
+                            if (!loadLocation.exists() || !InternetUtils.getChecksumOfFile(loadLocation.getPath()).equals(json.get("checksum_loader").getAsString())) {
+                                System.out.println("Downloading / updating OneCore loader...");
+                                if (!InternetUtils.download(json.get("loader").getAsString(), loadLocation)) {
+                                    if (!loadLocation.exists()) {
+                                        showErrorScreen();
+                                    }
+                                }
+                            }
+                        } else {
+                            // oh
+                            if (!loadLocation.exists()) {
+                                showErrorScreen();
+                            }
+                        }
+                    }
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                    if (!loadLocation.exists()) {
+                        showErrorScreen();
+                    }
                 }
-            } catch (Throwable e) {
-                e.printStackTrace();
-                if (!loadLocation.exists()) {
+
+                try {
+                    URL fileURL = loadLocation.toURI().toURL();
+                    if (!Launch.classLoader.getSources().contains(fileURL)) {
+                        Launch.classLoader.addURL(fileURL);
+                    }
+                    loader = ((ITweaker) Launch.classLoader.findClass(json == null ? "cc.woverflow.onecore.loader.OneCoreLoader" : json.getAsJsonObject("classpath").get("loader").getAsString()).newInstance());
+                } catch (Throwable e) {
+                    e.printStackTrace();
                     showErrorScreen();
                 }
             }
-
-            try {
-                URL fileURL = loadLocation.toURI().toURL();
-                if (!Launch.classLoader.getSources().contains(fileURL)) {
-                    Launch.classLoader.addURL(fileURL);
-                }
-                loader = ((ITweaker) Launch.classLoader.findClass(json == null ? "cc.woverflow.onecore.loader.OneCoreLoader" : json.getAsJsonObject("classpath").get("loader").getAsString()).newInstance());
-            } catch (Throwable e) {
-                e.printStackTrace();
-                showErrorScreen();
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            showErrorScreen();
         }
         if (loader != null) {
             loader.injectIntoClassLoader(classLoader);
